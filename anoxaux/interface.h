1bfd7307e03ae9cb1c802efc83a3b2e5259600de
Mark Sowden <markelswo@gmail.com>
2019-07-03 00:17:56 +0100

    added project files and other crap

---
+++ anoxaux/interface.h
@@ -0,0 +1,42 @@
+/*
+Copyright (C) 2019 Mark Sowden <markelswo@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#pragma once
+
+typedef unsigned long AFSFileHandle;
+typedef void* UnknownType;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  void afsDumpLoadedFiles(void);
+  char* afsGetLastError(void);
+  char* afsGetFileAttr(char* path);
+  AFSFileHandle afsOpenFile(char* s1, char* s2);
+  int afsFreeFile(AFSFileHandle* fh);
+  int afsWriteFile(AFSFileHandle fh, UnknownType param_2, UnknownType param_3);
+
+  int auxPerlHash(char* hash);
+  int auxSprintf(char* str, size_t size, char* format, ...);
+
+#ifdef __cplusplus
+};
+#endif
--- win32/sys_win.c
+++ win32/sys_win.c
@@ -443,7 +443,9 @@ void *Sys_GetGameAPI(void *parms) {
     Com_Error(ERR_FATAL, "Sys_GetGameAPI without Sys_UnloadingGame");
 
   // check the current debug directory first for development purposes
-  _getcwd(cwd, sizeof(cwd));
+  if (_getcwd(cwd, sizeof(cwd)) == NULL) {
+    Com_Error(ERR_FATAL, "Failed to get current working directory!\n");
+  }
   Com_sprintf(name, sizeof(name), "%s/%s/%s", cwd, debugdir, gamename);
   game_library = LoadLibrary(name);
   if (game_library) {
@@ -570,8 +572,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
       newtime = Sys_Milliseconds();
       time = newtime - oldtime;
     } while (time < 1);
-    //			Con_Printf ("time:%5.2f - %5.2f = %5.2f\n", newtime, oldtime,
-    //time);
+    //			Con_Printf ("time:%5.2f - %5.2f = %5.2f\n", newtime,
+    //oldtime, time);
 
     //	_controlfp( ~( _EM_ZERODIVIDE /*| _EM_INVALID*/ ), _MCW_EM );
     _controlfp(_PC_24, _MCW_PC);
